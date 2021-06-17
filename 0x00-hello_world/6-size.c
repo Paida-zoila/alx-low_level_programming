1b85744c7e3f539ec8f6767546e95545397a637f
Paida-zoila <paidachinama@gmail.com>
2021-06-17 10:35:19 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void) 
+int main(void)
+/**
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
+*/
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
