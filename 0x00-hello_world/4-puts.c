cf4c98c57284fac77af05f0043f52a9cf04c02b5
Paida-zoila <paidachinama@gmail.com>
2021-06-17 11:00:58 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 
 /**
@@ -9,6 +7,6 @@
 
 int main(void)  
 {
-puts("\"Programming is like building a multilingual puzzle");  
-return (0);  
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
